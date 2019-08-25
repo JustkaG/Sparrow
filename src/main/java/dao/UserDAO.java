7a66f6103f9c6c83565aa6624a7c89eefc99add8
JustynaG88 <wardaszka.justyna@gmail.com>
2019-08-25 10:04:14 +0200

    Database connection tested. Simple servlet added.

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -1,4 +1,33 @@
 package dao;
 
-public class UserDAO {
+import model.User;
+
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class UserDAO extends AbstractDAO {
+
+    public List<User> list() {
+
+        return entityManager.createQuery("select u from User u").getResultList();
+    }
+
+    public void createUser(User user) {
+        hibernateUtil.save(user);
+    }
+
+    public void deleteUser(long userId) {
+        hibernateUtil.delete(User.class, userId);
+    }
+
+    public List<User> getUserByLastName(String lastName) {
+        Query query = entityManager.createQuery("select u from User u where u.lastName = :lastName", User.class);
+         return query.setParameter("lastName", lastName).getResultList();
+    }
+
+    public User getUserByEmail(String email){
+        TypedQuery<User> query = entityManager.createQuery("select u from User u where u.email = :email", User.class);
+        return query.setParameter("email", email).getSingleResult();
+    }
 }
